66ef7d0d9c93b6405159651f7efe4df7b9dac09d
Diego Cantoral <diegocg1818@gmail.com>
2024-08-27 13:52:28 -0600

    Vista signup profesores

--- src/main/java/com/uam/UamCompartido/Controller/AuthController.java
+++ src/main/java/com/uam/UamCompartido/Controller/AuthController.java
@@ -42,7 +42,7 @@ public class AuthController {
 
     @GetMapping("/login")
     public String login(){
-        return "acercade";
+        return "signupProfesores";
     }
 
     @PostMapping("/login")
