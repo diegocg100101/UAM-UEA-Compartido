189723d3c5e0a3e98ee38b3496077b6558320f44
IlseMarilu <2203026744@correo.ler.uam.mx>
2024-08-27 20:59:59 -0600

    Login
    Vista de para ingresar

--- src/main/java/com/uam/UamCompartido/Controller/AuthController.java
+++ src/main/java/com/uam/UamCompartido/Controller/AuthController.java
@@ -42,7 +42,7 @@ public class AuthController {
 
     @GetMapping("/login")
     public String login(){
-        return "index";
+        return "loginProfesores";
     }
 
     @PostMapping("/login")
