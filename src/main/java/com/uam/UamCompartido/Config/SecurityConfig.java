b4e070f1a1a13bbcd7c79d2db7cf11c1f2137011
Diego Cantoral <diegocg1818@gmail.com>
2024-12-15 21:55:30 -0600

    cambios cambios

--- src/main/java/com/uam/UamCompartido/Config/SecurityConfig.java
+++ src/main/java/com/uam/UamCompartido/Config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/grupos/**").authenticated()
                         .requestMatchers("/user/**").hasAnyRole("ADMIN", "USER")
                         .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated()
